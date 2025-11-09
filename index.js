cec1a76439a2ed405062ca489902599c859ee00f
Hariom Patel <hariom70002582@gmail.com>
2025-11-09 22:06:51 +0530

    Sign Up functionality Implementation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require('./db/config');
+const cors = require('cors');
+
 const User = require('./db/User');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/register',async (req,res)=>{
     let user = new User(req.body);
     let result = await user.save();
+    console.log(result);
     res.send(result);
 })
 

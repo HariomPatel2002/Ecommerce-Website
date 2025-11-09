a19738b54c38bfa230622b3842f86ec769a2456f
Hariom Patel <hariom70002582@gmail.com>
2025-11-09 22:06:16 +0530

    Singup Functionality Implementation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link } from 'react-router-dom';
 
 const Nav=()=>{
+  const auth = localStorage.getItem("user");
  return (
     <div>
       <ul className='nav-ul'>
         <li className='nav-li'><Link to="/">Product</Link></li>
         <li className='nav-li'><Link to="/add">Add Product</Link></li>
         <li className='nav-li'><Link to="/update">Update Product</Link></li>
-        <li className='nav-li'><Link to="/Logout">Logout</Link></li>
         <li className='nav-li'><Link to="/profile">Profile</Link></li>
-        <li className='nav-li'><Link to="/SignUp">SignUp</Link></li>
+        <li>{auth ? <Link to="/logout">Logout</Link> : 
+        <Link to="/SignUp">Sign Up</Link>}</li>
       </ul>
     </div>
   );
